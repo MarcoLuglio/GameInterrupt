1f54588524cee6034edc77a720c54858e83aff4a
Marco Luglio <marcodejulho@gmail.com>
2020-06-29 22:41:38 -0300

    Adding common files to .gitignore

---
+++ Mac/GameInterrupt/GameInterrupt/GameInterruptApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameInterruptApp.swift
+//  GameInterrupt
+//
+//  Created by Marco Luglio on 29/06/20.
+//
+
+import SwiftUI
+
+@main
+struct GameInterruptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
